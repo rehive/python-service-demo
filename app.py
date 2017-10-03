92fc84549efcf96a0b71caf10d4197f5d5065725
OdinsPlasmaRifle <generalaetius.jbvb@gmail.com>
2017-10-03 17:24:44 +0200

    Added new heroku configuration and requirements

---
+++ app.py
@@ -0,0 +1,58 @@
+from flask import Flask, Response, request
+from rehive import Rehive, APIException
+
+API_TOKEN = "67582d9715354b0970400d5467762d6a13dbf106cf6180b718bcfc63099d9712"
+
+app = Flask(__name__)
+rehive = Rehive(API_TOKEN)
+
+
+@app.route('/')
+def index():
+	return "Rehive Demo"
+
+
+@app.route('/webhook/transaction/', methods=['POST'])
+def webhook_transaction():
+	content = request.get_json(silent=True)
+
+	tx_type = content['data']['tx_type']
+	source_transaction = content['data']['source_transaction']
+	status = content['data']['status']
+	amount = content['data']['amount']
+	account_reference = content['data']['account']
+	currency_code = content['data']['currency']['code']
+	user_identifier = content['data']['user']['identifier']
+
+	if status == "Complete" and source_transaction is None:
+		accounts = rehive.admin.accounts.get(
+			filters={"name": "savings", "user": user_identifier})
+
+		if len(accounts) > 0:
+			credit_account_reference = accounts[0]['reference']
+		else:
+			credit_account = rehive.admin.accounts.create(
+			    name="savings",
+			    primary=False,
+			    user=user_identifier
+			)
+
+			credit_account_reference = account['reference']
+
+		transaction = rehive.admin.transactions.create_transfer(
+			user=user_identifier,
+			recipient=user_identifier,
+			debit_account=account_reference,
+			credit_account=credit_account_reference,
+			currency=currency_code,
+			amount=int((10 * amount) / 100)
+		)
+
+		print(transaction)
+
+	return Response("{'status':'success'}", status=200, 
+		mimetype='application/json')
+
+
+if __name__ == "__main__":
+	app.run() 
